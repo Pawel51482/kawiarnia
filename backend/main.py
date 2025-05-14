10fff305ab77b3f3dd649f359b5a7cf1212cf2f4
fifi <djgamez1222@gmail.com>
2025-05-15 00:08:09 +0200

    Creating endpoints for reservations and orders and implementing logic from crud.py to them

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI, Depends, HTTPException
 from backend.database import get_db, init_db, engine
-from backend.models import Coffee
-from backend.schemas import UserCreate, UserLogin, Token
-from backend.crud import get_coffees, add_coffee, get_coffee_by_id, update_coffee, delete_coffee, register_user, login_user, get_current_user
+from backend.models import Coffee, User, Order, Reservation
+from backend.schemas import UserCreate, UserLogin, Token, OrderRead, OrderCreate, ReservationRead, ReservationCreate
+from backend.crud import (
+    get_coffees, 
+    add_coffee, 
+    get_coffee_by_id, 
+    update_coffee, 
+    delete_coffee, 
+    register_user, 
+    login_user, 
+    get_current_user,
+    create_order, 
+    get_orders_by_user,
+    create_reservation, 
+    get_reservations_by_user
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 
 # Tworzenie aplikacji FastAPI
@@ -46,3 +59,25 @@ async def login_endpoint(user_data: UserLogin, db: AsyncSession = Depends(get_db
 @app.get("/me")
 async def profile_endpoint(current_user = Depends(get_current_user)):
     return {"username": current_user.username}
+
+@app.post("/orders/", response_model=OrderRead)
+async def make_order(order_data: OrderCreate, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return await create_order(db, user_id=current_user.id, coffee_id=order_data.coffee_id, quantity=order_data.quantity)
+
+@app.get("/orders/", response_model=list[OrderRead])
+async def list_user_orders(db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return await get_orders_by_user(db, user_id=current_user.id)
+
+@app.post("/reservations/", response_model=ReservationRead)
+async def make_reservation(reservation_data: ReservationCreate, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return await create_reservation(
+        db,
+        user_id=current_user.id,
+        table_number=reservation_data.table_number,
+        date=reservation_data.date,
+        time=reservation_data.time,
+    )
+
+@app.get("/reservations/", response_model=list[ReservationRead])
+async def list_user_reservations(db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return await get_reservations_by_user(db, user_id=current_user.id)
